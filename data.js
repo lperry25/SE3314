50e5c8342e489b3838a773fdcf695298680e2e5e
Laura Perry <lauramperry.12@gmail.com>
2015-03-02 15:16:51 -0500

    added image files given by Ouda

--- data.js
+++ data.js
@@ -21,5 +21,33 @@ StockApp.StocksStateSummary.FIXTURES =
             changeValue: 0.39,
             changePercent: 0.0037,
             shareVolume: 43432023
+        },
+        {
+            id: 3,
+            name: "Facebook, Inc. (FB)",
+            openPrice: 74.98,
+            currentPrice: "",
+            changeValue: "",
+            changePercent: "",
+            shareVolume: ""
+
+        },
+        {
+            id: 4,
+            name: "Cisco Systems, Inc.",
+            openPrice: 27.41,
+            currentPrice: "",
+            changeValue: "",
+            changePercent: "",
+            shareVolume: ""
+        },
+        {
+            id: 5,
+            name: "Intel Corporation",
+            openPrice: 0.55,
+            currentPrice: "",
+            changeValue: "",
+            changePercent: "",
+            shareVolume: ""
         }
     ];
