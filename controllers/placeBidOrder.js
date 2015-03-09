96c3a6662a9b0cd4dcea01891092403127dc8558
Danica Mcleod <dmcleo2@uwo.ca>
2015-03-09 02:00:37 -0400

    transactions

--- controllers/placeBidOrder.js
+++ controllers/placeBidOrder.js
@@ -8,15 +8,67 @@ StockApp.PlaceBidOrderController = Ember.Controller.extend({
 
             console.log("inside PlaceBidOrderController submit, shares: "+shares+", price: "+price);
 
-            // Create the new buyOrder model
-            var buyOrder = this.store.createRecord('buyOrder', {
-                numberShares: shares,
-                purchasePrice: price,
-                company: model
-            });
+            var sellOrders = model.get('sellOrders').sortBy('sellOrders.price:asc');
 
-            // Save the new model
-            buyOrder.save();
+            for (var i = 0; i < sellOrders.length; i++) {
+                // transaction if bidder willing to pay more than or as much as selling price
+                if (price >= sellOrders[i].get('salePrice')) {
+                    model.set('currentPrice', price); // change currentPrice to bid price
+
+                    // determine remaining number of shares left on each order and adjust/delete orders as necessary
+                    if (sellOrders[i].get('numberShares') > shares) {
+                        console.log('bid order - numb shares less than sell order');
+
+                        // subtract the shares sold from the sell order
+                        sellOrders[i].set('numberShares', sellOrders[i].get('numberShares') - shares);
+
+                        // set shares for bid order to 0 (all bought)
+                        shares = 0;
+
+                        // no more transactions to make
+                        break;
+                    }
+                    else if (sellOrders[i].get('numberShares') == shares) {
+                        console.log('bid order - numb shares equal to sell order');
+
+                        // delete the sell order
+                        this.store.find('sellOrder', sellOrders[i].get('id')).then(function (order) {
+                            order.deleteRecord();
+                            order.save();
+                        });
+
+                        // set shares for bid order to 0 (all bought)
+                        shares = 0;
+
+                        // no more transactions to make
+                        break;
+                    }
+                    else if (sellOrders[i].get('numberShares') < shares) {
+                        console.log('bid order - numb shares greater than sell order');
+
+                        // adjusts the number of shares to reflect number sold
+                        shares = shares - sellOrders[i].get('numberShares');
+
+                        // delete the buy order
+                        this.store.find('sellOrder', sellOrders[i].get('id')).then(function(order) {
+                            order.deleteRecord();
+                            order.save();
+                        });
+                    }
+                }
+            }
+
+            if (shares > 0) {
+                // Create the new buyOrder record (only if all shares not bought)
+                var buyOrder = this.store.createRecord('buyOrder', {
+                    numberShares: shares,
+                    purchasePrice: price,
+                    company: model
+                });
+
+                // Save the new record
+                buyOrder.save();
+            }
 
             // clear the text fields
             this.set('shares', '');
--- controllers/placeSellOrder.js
+++ controllers/placeSellOrder.js
@@ -8,15 +8,67 @@ StockApp.PlaceSellOrderController = Ember.Controller.extend({
 
             console.log("inside PlaceSellOrderController submit, shares: "+shares+", price: "+price);
 
-            // Create the new sellOrder model
-            var sellOrder = this.store.createRecord('sellOrder', {
-                numberShares: shares,
-                salePrice: price,
-                company: model
-            });
+            var buyOrders = model.get('buyOrders').sortBy('buyOrders.price:desc');
 
-            // Save the new model
-            sellOrder.save();
+            for (var i = 0; i < buyOrders.length; i++) {
+                // transaction if bidder willing to pay more than or as much as selling price
+                if (buyOrders[i].get('purchasePrice') >= price) {
+                    model.set('currentPrice', price); // change currentPrice to sale price
+
+                    // determine remaining number of shares left on each order and adjust/delete orders as necessary
+                    if (buyOrders[i].get('numberShares') > shares) {
+                        console.log('sell order - numb shares less than buy order');
+
+                        // subtract the shares sold from the buy order
+                        buyOrders[i].set('numberShares', buyOrders[i].get('numberShares') - shares);
+
+                        // set shares for sell order to 0 (all sold)
+                        shares = 0;
+
+                        // no more transactions to make
+                        break;
+                    }
+                    else if (buyOrders[i].get('numberShares') == shares) {
+                        console.log('sell order - numb shares equal to buy order');
+
+                        // delete the buy order
+                        this.store.find('buyOrder', buyOrders[i].get('id')).then(function (order) {
+                            order.deleteRecord();
+                            order.save();
+                        });
+
+                        // set shares for sell order to 0 (all sold)
+                        shares = 0;
+
+                        // no more transactions to make
+                        break;
+                    }
+                    else if (buyOrders[i].get('numberShares') < shares) {
+                        console.log('sell order - numb shares greater than buy order');
+
+                        // adjusts the number of shares to reflect number sold
+                        shares = shares - buyOrders[i].get('numberShares');
+
+                        // delete the buy order
+                        this.store.find('buyOrder', buyOrders[i].get('id')).then(function(order) {
+                            order.deleteRecord();
+                            order.save();
+                        });
+                    }
+                }
+            }
+
+            if (shares > 0) {
+                // Create the new sellOrder record (only if all shares not sold)
+                var sellOrder = this.store.createRecord('sellOrder', {
+                    numberShares: shares,
+                    salePrice: price,
+                    company: model
+                });
+
+                // Save the new record
+                sellOrder.save();
+            }
 
             // clear the text fields
             this.set('shares', '');
