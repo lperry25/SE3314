44c73aa1f88c4ec457e495afe8156e5fb02898a9
Danica Mcleod <dmcleo2@uwo.ca>
2015-03-10 21:31:56 -0400

    made company info auto displayed beneath stock state summ

--- routes/stocksStateSummary.js
+++ routes/stocksStateSummary.js
@@ -2,5 +2,8 @@ StockApp.StocksStateSummaryRoute = Ember.Route.extend({
     // model of all companies to populate stockStateSummary
     model: function(){
        return this.store.find("company");
+    },
+    afterModel: function(companies, transition) {
+        this.transitionTo("companyInfo", companies.get("firstObject"));
     }
 });
