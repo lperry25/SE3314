d80e1b559871d6491298629f9e3651e4f2853041
Danica Mcleod <dmcleo2@uwo.ca>
2015-03-10 21:45:01 -0400

    reverted auto display

--- routes/stocksStateSummary.js
+++ routes/stocksStateSummary.js
@@ -2,8 +2,5 @@ StockApp.StocksStateSummaryRoute = Ember.Route.extend({
     // model of all companies to populate stockStateSummary
     model: function(){
        return this.store.find("company");
-    },
-    afterModel: function(companies, transition) {
-        this.transitionTo("companyInfo", companies.get("firstObject"));
     }
 });
